831b99ad82be0d8921c53e865d7797fc3832324e
kyroven <84641399+kyroven@users.noreply.github.com>
2026-04-01 22:30:03 -0400

    mainly just reformatting, removing unused code, and adding some info comments

--- src/main.rs
+++ src/main.rs
@@ -16,20 +16,18 @@ slint::include_modules!();
 
 struct Media {
     file: FileHandle,
-    duration: Option<f64>,
     title: Option<String>,
     artist: Option<String>,
-    source: Option<Decoder<BufReader<File>>>,
+    _source: Option<Decoder<BufReader<File>>>,
 }
 impl Media {
     pub fn new(file: FileHandle) -> Self {
-        let (title, artist, duration) = read_metadata(file.path());
+        let (title, artist, _) = read_metadata(file.path());
         Self {
             file,
-            duration,
             title,
             artist,
-            source: None,
+            _source: None,
         }
     }
 
@@ -37,10 +35,11 @@ impl Media {
         self.file.path()
     }
 
+    // TODO do we need to save the source in the struct? we probably will only need it once
     /// Moves `source` out of self if it already exists, otherwise creates and returns new `source`
-    pub fn source(&mut self) -> Result<Decoder<BufReader<File>>, <Decoder<BufReader<File>> as TryFrom<BufReader<File>>>::Error> {
-        if let Some(_) = &mut self.source {
-            let res = std::mem::replace(&mut self.source, None);
+    pub fn _source(&mut self) -> Result<Decoder<BufReader<File>>, <Decoder<BufReader<File>> as TryFrom<BufReader<File>>>::Error> {
+        if let Some(_) = &mut self._source {
+            let res = std::mem::replace(&mut self._source, None);
             return Ok(res.unwrap())
         } else {
             return self.create_source()
@@ -75,10 +74,11 @@ fn read_metadata(path: &Path) -> (Option<String>, Option<String>, Option<f64>) {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // use slint::Model;
 
     let current_file: Arc<Mutex<Option<FileHandle>>> = Arc::new(Mutex::new(None));
-    let current_media: Arc<Mutex<Option<Media>>> = Arc::new(Mutex::new(None));
+    // TODO do we actually need to save the current media like this? or can we just pass the info
+    // to the ui and then be done with it?
+    let _current_media: Arc<Mutex<Option<Media>>> = Arc::new(Mutex::new(None));
     let current_time_update = Arc::new(Timer::default());
 
     // let current_length: Arc<Mutex<Option<Duration>>> = Arc::new(Mutex::new(None));
@@ -94,102 +94,101 @@ fn main() -> Result<(), Box<dyn Error>> {
     ui.on_play_button({
         let ui_handle = ui.as_weak();
         let current_file_handle = Arc::clone(&current_file);
-        let audio_player_handle = Arc::clone(&audio_player);
+        let player_handle = Arc::clone(&audio_player);
         move || {
             let ui = ui_handle.unwrap();
 
-            let val = current_file_handle.lock().unwrap();
-            if let Some(_) = *val {
-                if !audio_player_handle.is_paused() {
-                    audio_player_handle.pause();
+            if current_file_handle.lock().unwrap().is_some() {
+                if !player_handle.is_paused() {
+                    player_handle.pause();
                     ui.set_media_playing(false);
                     
                 } else {
-                    audio_player_handle.play();
+                    player_handle.play();
                     ui.set_media_playing(true);
                 }
             }
         }
     });
 
-    ui.on_file_button({
-        let ui_handle = ui.as_weak();
-        let audio_player = Arc::clone(&audio_player);
-        let current_media_handle = Arc::clone(&current_media);
-        current_time_update.start(TimerMode::Repeated, Duration::from_millis(50), {
-            let ui_handle = ui.as_weak();
-            let player_handle = Arc::clone(&audio_player);
-            move || {
-                let ui = ui_handle.unwrap();
-                if ui.get_media_playing() {
-                    // println!("--------------");
-                    // println!("current time: {}", ui.get_current_time());
-                    let pos = player_handle.get_pos().as_millis() as i32;
-                    ui.set_current_time(player_handle.get_pos().as_millis() as i32);
-                    // println!("current_time_update: {pos}");
-                }
-        }});
-        move || {
-            let current_file_handle = Arc::clone(&current_file);
-            let ui = ui_handle.unwrap();
-            let audio_player_handle = Arc::clone(&audio_player);
-            let current_media_handle = Arc::clone(&current_media_handle);
-            slint::spawn_local(async move {
-                let file = AsyncFileDialog::new()
-                    .add_filter("audio", &["ogg", "wav", "flac", "mp3"])
-                    .set_directory("/")
-                    .pick_file()
-                    .await;
-                if let Some(handle) = file {
-                    let mut current_file = current_file_handle.lock().unwrap();
-                    let mut current_media = current_media_handle.lock().unwrap();
-                    *current_file = Some(handle.clone());
-                    let new_media = Media::new(handle);
-                    set_metadata(&ui, &new_media);
-                    let source = new_media.create_source().unwrap();
-                    if let Some(duration) = source.total_duration() {
-                        ui.set_total_duration(duration.as_millis() as i32);
-                    };
-                    audio_player_handle.clear();
-                    audio_player_handle.append(source);
-                    ui.set_current_time(audio_player_handle.get_pos().as_millis() as i32);
-                    // ui.set_media_artist()
-                    ui.set_file_selected(true);
-                }
-            }).unwrap();
-    }});
-
-    ui.on_change_volume({
-        let ui_handle = ui.as_weak();
-        let audio_player_handle = Arc::clone(&audio_player);
-        move || {
-            let ui = ui_handle.unwrap();
-            audio_player_handle.set_volume(ui.get_volume_level());
+    // This timer is responsible for continuously reading the current song position from the audio
+    // player, and then updating the ui seek bar such that it follows the current position of the
+    // song
+    let ui_handle = ui.as_weak();
+    let player_handle = Arc::clone(&audio_player);
+    current_time_update.start(TimerMode::Repeated, Duration::from_millis(50), move || {
+        let ui = ui_handle.unwrap();
+        if ui.get_media_playing() {
+            // println!("--------------");
+            // println!("current time: {}", ui.get_current_time());
+            ui.set_current_time(player_handle.get_pos().as_millis() as i32);
+            // println!("current_time_update: {}", player_handle.get_pos().as_millis() as i32);
         }
     });
+    
+    
+    let ui_handle = ui.as_weak();
+    let player_handle = Arc::clone(&audio_player);
+    ui.on_file_button(move || {
+        let current_file_handle = Arc::clone(&current_file);
+        let ui = ui_handle.unwrap();
+        let player_handle = Arc::clone(&player_handle);
+        slint::spawn_local(async move {
+            let file = AsyncFileDialog::new()
+                .add_filter("audio", &["ogg", "wav", "flac", "mp3"])
+                .set_directory("/")
+                .pick_file()
+                .await;
+            if let Some(handle) = file {
+                let mut current_file = current_file_handle.lock().unwrap();
+                *current_file = Some(handle.clone());
+                let new_media = Media::new(handle);
+                set_metadata(&ui, &new_media);
+                let source = new_media.create_source().unwrap();
+                if let Some(duration) = source.total_duration() {
+                    ui.set_total_duration(duration.as_millis() as i32);
+                };
+                player_handle.clear();
+                player_handle.append(source);
+                ui.set_current_time(player_handle.get_pos().as_millis() as i32);
+                // ui.set_media_artist()
+                ui.set_file_selected(true);
+            }
+        }).unwrap();
+    });
 
-    ui.on_seek_press({
-        let time_update_handle = Arc::clone(&current_time_update);
-        move || {
-            time_update_handle.stop();
-    }});
+    let ui_handle = ui.as_weak();
+    let audio_player_handle = Arc::clone(&audio_player);
+    ui.on_change_volume(move || {
+        let ui = ui_handle.unwrap();
+        audio_player_handle.set_volume(ui.get_volume_level());
+    });
 
-    ui.on_seek({
-        let time_update_handle = Arc::clone(&current_time_update);
-        // time_update_handle.stop();
-        let player_handle = Arc::clone(&audio_player);
-        let ui_handle = ui.as_weak();
-        move |time| {
-            let ui = ui_handle.unwrap();
-            player_handle.try_seek(Duration::from_millis(time.try_into().unwrap())).unwrap();
-            time_update_handle.restart();
-            let last = ui.get_resume_update_flag();
-            ui.set_resume_update_flag(last + 1);
-    }});
+    // Pauses the timer that continuously updates the seek bar with current song position whenever
+    // the user starts dragging the seek bar. The timer must be restarted later, after the user
+    // finishes dragging the bar and we seek to position
+    let time_update_handle = Arc::clone(&current_time_update);
+    ui.on_seek_press(move || {
+        time_update_handle.stop();
+    });
+
+    // Call try_seek from the audio player, then restart the timer that continuously updates the seek
+    // bar with current position. This is required because when the user starts dragging the seek bar
+    // we pause the update timer so that it doesn't keep trying to update the slider value while the
+    // user is holding it in place
+    let time_update_handle = Arc::clone(&current_time_update);
+    let player_handle = Arc::clone(&audio_player);
+    let ui_handle = ui.as_weak();
+    ui.on_seek(move |time| {
+        let ui = ui_handle.unwrap();
+        player_handle.try_seek(Duration::from_millis(time.try_into().unwrap())).unwrap();
+        time_update_handle.restart();
+        let last = ui.get_resume_update_flag();
+        ui.set_resume_update_flag(last + 1);
+    });
     
     ui.run()?;
     
-
     Ok(())
 }
 
